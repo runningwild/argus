b3b8e16752ba5ff1054951ce405804a906c4c2d7
Jonathan Wills <runningwild@gmail.com>
2014-09-21 16:08:37 -0700

    Fixed power functions

--- utils/power.go
+++ utils/power.go
@@ -5,19 +5,21 @@ import (
 )
 
 func Power(a, b *image.RGBA, x0, y0 int, mppp float64) (pow float64, over bool) {
-	var p float64
+	pow = 0
 	for y := y0; y < y0+8; y++ {
-		for x := x0; x < x0+8; x++ {
-			offset := a.PixOffset(x, y)
-			rdiff := float64(a.Pix[offset+0]) - float64(b.Pix[offset+0])
-			gdiff := float64(a.Pix[offset+1]) - float64(b.Pix[offset+1])
-			bdiff := float64(a.Pix[offset+2]) - float64(b.Pix[offset+2])
-			add := rdiff*rdiff + gdiff*gdiff + bdiff*bdiff
-			// if add > mppp {
-			// 	return add, true
-			// }
-			p += add
-		}
+		offset := a.PixOffset(x0, y)
+		pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
 	}
-	return p, p > mppp*64
+	over = pow > mppp*64
+	return
+}
+
+func PowerAsm(a, b *image.RGBA, x0, y0 int, mppp float64) (pow float64, over bool) {
+	pow = 0
+	for y := y0; y < y0+8; y++ {
+		offset := a.PixOffset(x0, y)
+		pow += PowerLineAsm(a.Pix[offset:], b.Pix[offset:])
+	}
+	over = pow > mppp*64
+	return
 }
--- utils/power_test.go
+++ utils/power_test.go
@@ -6,6 +6,32 @@ import (
 	"testing"
 )
 
+func BenchmarkSimple(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		utils.Foo(i)
+	}
+}
+
+func TestSimple(t *testing.T) {
+	foo1 := utils.Foo(1)
+	if foo1 != 2 {
+		t.Errorf("Foo(1) returned %v failed", foo1)
+	}
+	foo12 := utils.Foo(12)
+	if foo12 != 13 {
+		t.Errorf("Foo(12) returned %v failed", foo12)
+	}
+	foom100 := utils.Foo(-100)
+	if foom100 != -99 {
+		t.Errorf("Foo(-100) returned %v failed", foom100)
+	}
+	foo7777 := utils.Foo(7777)
+	if foo7777 != 7778 {
+		t.Errorf("Foo(7777) returned %v failed", foo7777)
+	}
+}
+
 func TestPower(t *testing.T) {
 	imgA := image.NewRGBA(image.Rect(0, 0, 10, 10))
 	imgB := image.NewRGBA(image.Rect(0, 0, 10, 10))
@@ -18,9 +44,10 @@ func TestPower(t *testing.T) {
 		t.Errorf("Expected not to be over.\n")
 	}
 
-	imgA.Pix[4+0] = 10
-	imgA.Pix[4+1] = 10
-	imgA.Pix[4+2] = 10
+	offset := imgA.PixOffset(0, 0)
+	imgA.Pix[offset+0] = 10
+	imgA.Pix[offset+1] = 10
+	imgA.Pix[offset+2] = 10
 	pow, over = utils.Power(imgA, imgB, 0, 0, 1)
 	if pow != 300 {
 		t.Errorf("Expected power of 300, got %v.\n", pow)
@@ -37,9 +64,14 @@ func TestPower(t *testing.T) {
 		t.Errorf("Expected not to be over.\n")
 	}
 
-	imgB.Pix[4+0] = 10
-	imgB.Pix[4+1] = 10
-	imgB.Pix[4+2] = 10
+	offset = imgA.PixOffset(0, 0)
+	imgB.Pix[offset+0] = 10
+	imgB.Pix[offset+1] = 10
+	imgB.Pix[offset+2] = 10
+	offset = imgA.PixOffset(9, 9)
+	imgB.Pix[offset+0] = 0
+	imgB.Pix[offset+1] = 10
+	imgB.Pix[offset+2] = 20
 	pow, over = utils.Power(imgA, imgB, 0, 0, 1)
 	if pow != 0 {
 		t.Errorf("Expected power of 0, got %v.\n", pow)
@@ -47,7 +79,83 @@ func TestPower(t *testing.T) {
 	if over {
 		t.Errorf("Expected not to be over.\n")
 	}
+	pow, over = utils.Power(imgA, imgB, 1, 1, 1)
+	if pow != 0 {
+		t.Errorf("Expected power of 0, got %v.\n", pow)
+	}
+	if over {
+		t.Errorf("Expected not to be over.\n")
+	}
+	pow, over = utils.Power(imgA, imgB, 2, 2, 1)
+	if pow != 500 {
+		t.Errorf("Expected power of 500, got %v.\n", pow)
+	}
+	if !over {
+		t.Errorf("Expected to be over.\n")
+	}
+}
 
+func TestPowerAsm(t *testing.T) {
+	imgA := image.NewRGBA(image.Rect(0, 0, 10, 10))
+	imgB := image.NewRGBA(image.Rect(0, 0, 10, 10))
+
+	pow, over := utils.PowerAsm(imgA, imgB, 0, 0, 1)
+	if pow != 0 {
+		t.Errorf("Expected power of 0, got %v.\n", pow)
+	}
+	if over {
+		t.Errorf("Expected not to be over.\n")
+	}
+
+	offset := imgA.PixOffset(0, 0)
+	imgA.Pix[offset+0] = 10
+	imgA.Pix[offset+1] = 10
+	imgA.Pix[offset+2] = 10
+	pow, over = utils.PowerAsm(imgA, imgB, 0, 0, 1)
+	if pow != 300 {
+		t.Errorf("Expected power of 300, got %v.\n", pow)
+	}
+	if !over {
+		t.Errorf("Expected to be over.\n")
+	}
+
+	pow, over = utils.PowerAsm(imgA, imgB, 1, 1, 1)
+	if pow != 0 {
+		t.Errorf("Expected power of 0, got %v.\n", pow)
+	}
+	if over {
+		t.Errorf("Expected not to be over.\n")
+	}
+
+	offset = imgA.PixOffset(0, 0)
+	imgB.Pix[offset+0] = 10
+	imgB.Pix[offset+1] = 10
+	imgB.Pix[offset+2] = 10
+	offset = imgA.PixOffset(9, 9)
+	imgB.Pix[offset+0] = 0
+	imgB.Pix[offset+1] = 10
+	imgB.Pix[offset+2] = 20
+	pow, over = utils.PowerAsm(imgA, imgB, 0, 0, 1)
+	if pow != 0 {
+		t.Errorf("Expected power of 0, got %v.\n", pow)
+	}
+	if over {
+		t.Errorf("Expected not to be over.\n")
+	}
+	pow, over = utils.PowerAsm(imgA, imgB, 1, 1, 1)
+	if pow != 0 {
+		t.Errorf("Expected power of 0, got %v.\n", pow)
+	}
+	if over {
+		t.Errorf("Expected not to be over.\n")
+	}
+	pow, over = utils.PowerAsm(imgA, imgB, 2, 2, 1)
+	if pow != 500 {
+		t.Errorf("Expected power of 500, got %v.\n", pow)
+	}
+	if !over {
+		t.Errorf("Expected to be over.\n")
+	}
 }
 
 func BenchmarkPower(b *testing.B) {
@@ -56,5 +164,14 @@ func BenchmarkPower(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		utils.Power(imgA, imgB, 0, 0, 100)
+	}
+}
+
+func BenchmarkPowerAsm(b *testing.B) {
+	imgA := image.NewRGBA(image.Rect(0, 0, 8, 8))
+	imgB := image.NewRGBA(image.Rect(0, 0, 8, 8))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		utils.PowerAsm(imgA, imgB, 0, 0, 100)
 	}
 }
