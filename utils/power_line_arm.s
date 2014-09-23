022e1154b33eb1b6ea7f8831b17e91941f060500
Jonathan Wills <runningwild@gmail.com>
2014-09-23 06:01:24 +0000

    fix rightshifts on arm

--- utils/power_line_arm.s
+++ utils/power_line_arm.s
@@ -5,151 +5,187 @@ TEXT	·PowerLine+0(SB),16,$36-32
 	MOVW	$·bRgba+12(FP),R1
 	MOVW	0(R1), R1		// R1 holds the address of the first element in array B
 
-	MOVW	$0, R8			// R8 will accumulate the power (RAWR!!)
+	// MOVW	$0, R8			// R8 will accumulate the power (RAWR!!)
 
-        MOVB    0(R0), R2
-        MOVB    0(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    0(R0), R2       // Store 4 bytes of A in R2
+        MOVW    0(R1), R3       // Store 4 bytes of B in R2
+        AND     $255, R2, R4    // Put the lowest byte of A in R4
+        AND     $255, R3, R5    // Put the lowest byte of B in R5
+        SUB     R4, R5, R4      // Compute the power and add it to R8
+        MUL     R4, R4, R8      // (This first one is can just be stored directly into R8)
+        // ADD     R4, R8, R8      
 
-        MOVB    1(R0), R2
-        MOVB    1(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2       // Shift by 8 to get the next byte and repeat for all bytes
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4      // Unlike above we have to store the temporary result and
+        ADD     R4, R8, R8      // add it to R8
 
-        MOVB    2(R0), R2
-        MOVB    2(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    3(R0), R2
-        MOVB    3(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R4
+        MOVW    R3>>8, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    4(R0), R2
-        MOVB    4(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    4(R0), R2       // Grab the next 4 bytes and repeat
+        MOVW    4(R1), R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    5(R0), R2
-        MOVB    5(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    6(R0), R2
-        MOVB    6(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    7(R0), R2
-        MOVB    7(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R4
+        MOVW    R3>>8, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    8(R0), R2
-        MOVB    8(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    8(R0), R2
+        MOVW    8(R1), R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    9(R0), R2
-        MOVB    9(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    10(R0), R2
-        MOVB    10(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    11(R0), R2
-        MOVB    11(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R4
+        MOVW    R3>>8, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    12(R0), R2
-        MOVB    12(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    12(R0), R2
+        MOVW    12(R1), R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    13(R0), R2
-        MOVB    13(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    14(R0), R2
-        MOVB    14(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    15(R0), R2
-        MOVB    15(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R4
+        MOVW    R3>>8, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    16(R0), R2
-        MOVB    16(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    16(R0), R2
+        MOVW    16(R1), R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    17(R0), R2
-        MOVB    17(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    18(R0), R2
-        MOVB    18(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    19(R0), R2
-        MOVB    19(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R4
+        MOVW    R3>>8, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    20(R0), R2
-        MOVB    20(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    20(R0), R2
+        MOVW    20(R1), R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    21(R0), R2
-        MOVB    21(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    22(R0), R2
-        MOVB    22(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R2
+        MOVW    R3>>8, R3
+        AND     $255, R2, R4
+        AND     $255, R3, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
-        MOVB    23(R0), R2
-        MOVB    23(R1), R3
-        SUB     R3, R2, R2
-        MUL     R2, R2, R2
-        ADD     R2, R8, R8
+        MOVW    R2>>8, R4
+        MOVW    R3>>8, R5
+        SUB     R4, R5, R4
+        MUL     R4, R4, R4
+        ADD     R4, R8, R8
 
 	MOVW	$0, R1
 	MOVW	R8,·r2+24(FP)		// FP+24 is the low part of the return value
