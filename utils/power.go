c47aa92664f9882a7c5c0640d78b2209bd95fce6
Jonathan Wills <runningwild@gmail.com>
2014-09-23 22:43:56 -0700

    Unrolled a loop

--- utils/power.go
+++ utils/power.go
@@ -6,10 +6,22 @@ import (
 
 func Power(a, b *rgb.Image, x0, y0 int, mppp uint64) (pow uint64, over bool) {
 	pow = 0
-	for y := y0; y < y0+8; y++ {
-		offset := a.PixOffset(x0, y)
-		pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
-	}
+	offset := a.PixOffset(x0, y0)
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
+	offset += a.Stride
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
+	offset += a.Stride
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
+	offset += a.Stride
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
+	offset += a.Stride
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
+	offset += a.Stride
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
+	offset += a.Stride
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
+	offset += a.Stride
+	pow += PowerLine(a.Pix[offset:], b.Pix[offset:])
 	over = pow > mppp*64
 	return
 }
